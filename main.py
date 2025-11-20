0eb37c869eb61f9b9652874b55513d56d890c6df
YukiNo <tahanacibe@gmail.com>
2025-11-20 11:48:31 +0100

    full downlaod basic script

---
+++ main.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import yt_dlp
+
+#* Settings
+VIDEO_BEST_QUALITY = "bestvideo+bestaudio/best"
+VIDEO_EXT = "mp4"
+
+
+AUDIO_BEST_QUALITY = 'bestaudio/best'
+AUDIO_EXT = "mp3"
+
+
+#* Download Audio from URLs
+def download_audio(urls, output_dir="Download", formate=VIDEO_BEST_QUALITY, ext=VIDEO_EXT, no_playlist=False):
+    
+    if is_video:
+        postprocessors = [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': ext,
+        }]
+    else:
+        postprocessors = [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': ext,      
+            'preferredquality': '192',    
+        }]
+    # handle output directory
+    ydl_opts = {
+        'format': formate,
+        'postprocessors': postprocessors,
+        'outtmpl': '%(title)s.%(ext)s', 
+        'paths':{
+            'home': output_dir,
+            'temp': os.path.abspath(f'{output_dir}/Temp/')
+        },
+        'noplaylist': no_playlist,
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        for url in urls:
+            try:
+                print(f"[TARGET]: {url}")
+                ydl.download([url])
+                print(f"[DONE]: {url}")
+            except Exception as e:
+                print(f"[ERROR]/ {url}: {str(e)}")
+
+
+# run script
+if __name__ == "__main__":
+    # settings
+    is_video = True
+    no_playlist = True
+    
+    # output directory
+    root_output_dir = "Download"
+    audio_dir = root_output_dir + "/Audio"
+    video_dir = root_output_dir + "/Video"
+    
+    # download settings
+    urls = ["https://www.tiktok.com/@zubsae/video/7573424844323228950?is_from_webapp=1&sender_device=pc"]
+    formate = VIDEO_BEST_QUALITY if is_video else AUDIO_BEST_QUALITY
+    ext = VIDEO_EXT if is_video else AUDIO_EXT
+    output_dir = video_dir if is_video else audio_dir
+    download_audio(urls, output_dir, formate, ext, no_playlist)
